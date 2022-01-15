6641efec22937771beb1fa724de165587ec2969b
Sebastian Kreft <sebastian.kreft.sg@gmail.com>
2022-01-15 17:23:25 +0100

    Add sorting products for pages

--- logic.js
+++ logic.js
@@ -10,12 +10,57 @@ class Product {
 }
 
 
-const product1 = new Product(1, "Huang Xiao", "Yellow Tea", "pictures/yellow-pics/huang-xiao.jpg", 20);
-const product2 = new Product(2, "Golden Dragon Organic", "Yellow Tea", "pictures/yellow-pics/golden-dragon-organic.jpg", 39);
-const product3 = new Product(3, "Junshan Yin Zhen", "Yellow Tea", "pictures/yellow-pics/junshan-yin-zhen-2021.jpg", 175);
+var product1 = new Product(1, "Huang Xiao", "Yellow Tea", "pictures/yellow-pics/huang-xiao.jpg", 20);
+var product2 = new Product(2, "Golden Dragon Organic", "Yellow Tea", "pictures/yellow-pics/golden-dragon-organic.jpg", 39);
+var product3 = new Product(3, "Junshan Yin Zhen", "Yellow Tea", "pictures/yellow-pics/junshan-yin-zhen-2021.jpg", 175);
 
+var product4 = new Product(4, "Bangdong Gushu Zi Ya 2021", "White Tea", "pictures/white-pics/bangdong-gushu-zi-ya.jpg", 140);
+var product5 = new Product(5, "China White Snow Buds", "White Tea", "pictures/white-pics/china_white_snow_buds.jpg", 22);
+var product6 = new Product(6, "Bai Mu Dan Supergrade", "White Tea", "pictures/white-pics/bai-mu-dan-supergrade-2020.jpg", 27);
+var product7 = new Product(7, "Ya Bao Yesheng", "White Tea", "pictures/white-pics/ya-bao-yesheng.jpg", 39);
+var product8 = new Product(8, "Mingfeng Shan Gushu Baicha", "White Tea", "pictures/white-pics/mingfeng-shan-gushu-baicha.jpg", 45);
+var product9 = new Product(9, "Guangxi Silver Needle", "White Tea", "pictures/white-pics/guangxi-silver-needle.jpg", 47);
+var product10 = new Product(10, "Taiwan Baicha 2021", "White Tea", "pictures/white-pics/taiwan-baicha.jpg", 68);
+var product11 = new Product(11, "Malawi White Peony", "White Tea", "pictures/white-pics/malawi-white-peony.jpg", 29);
+var product12 = new Product(12, "Nepal Bai Mu Dan", "White Tea", "pictures/white-pics/nepal-bai-mu-dan.jpg", 31);
+var product13 = new Product(13, "Bai Hao Yin Zhen Premium", "White Tea", "pictures/white-pics/bai-hao-yin-zhen-premium.jpg", 110);
 
-let products = new Array(product1, product2, product3);
+var product14 = new Product(14, "Guizhou Lubaoshi", "Green Tea", "pictures/green-pics/guizhou-lubaoshi.jpg", 17);
+var product15 = new Product(15, "Huangshan Mao Feng 2021", "Green Tea", "pictures/green-pics/huangshan-mao-feng-2021.jpg", 110);
+var product16 = new Product(16, "Guyu Long Jing 2021", "Green Tea", "pictures/green-pics/guyu-long-jing-2021.jpg", 42);
+var product17 = new Product(17, "Anji Bai Cha 2021", "Green Tea", "pictures/green-pics/anji-bai-cha-2021.jpg", 79);
+var product18 = new Product(18, "Sencha Isagawa Organic 2021", "Green Tea", "pictures/green-pics/sencha-isagawa-organic-2021.jpg", 44);
+var product19 = new Product(19, "Yunnan Zhu Cha", "Green Tea", "pictures/green-pics/yunnan-zhu-cha.jpg", 10);
+var product20 = new Product(20, "Genmaicha Uji", "Green Tea", "pictures/green-pics/genmaicha-uji.jpg", 28);
+var product21 = new Product(21, "Sichuan Luo Cha 2021", "Green Tea", "pictures/green-pics/sichuan-luo-cha-2021.jpg", 24);
+var product22 = new Product(22, "Mengding Gan Lu 2021", "Green Tea", "pictures/green-pics/mengding-gan-lu-2021.jpg", 55);
+var product23 = new Product(23, "Sencha", "Green Tea", "pictures/green-pics/sencha.jpg", 15);
+
+var product24 = new Product(24, "Wild Yunnan Moonlight Organic", "Black Tea", "pictures/black-pics/wild-yunnan-moonlight-organic.jpg", 24);
+var product25 = new Product(25, "Wuyi Qizhong Hongcha 2020", "Black Tea", "pictures/black-pics/wuyi-qizhong-hongcha-2020.jpg", 110);
+var product26 = new Product(26, "Tanyang Gongfu 2021", "Black Tea", "pictures/black-pics/tanyang-gongfu-2021.jpg", 59);
+var product27 = new Product(27, "Yunnan Gold", "Black Tea", "pictures/black-pics/yunnan-zloty.jpg", 13);
+var product28 = new Product(28, "Hong Yu 2021", "Black Tea", "pictures/black-pics/hong-yu-2021.jpg", 68);
+var product29 = new Product(29, "Shimen Hongcha 2021", "Black Tea", "pictures/black-pics/shimen-hongcha-2021.jpg", 107);
+var product30 = new Product(30, "Wakocha Koshun 2021", "Black Tea", "pictures/black-pics/wakocha-koshun-2021.jpg", 49);
+var product31 = new Product(31, "Taiwan Breakfast", "Black Tea", "pictures/black-pics/wakocha-koshun-2021.jpg", 20);
+var product32 = new Product(32, "Keemun Imperial", "Black Tea", "pictures/black-pics/keemun-imperial.jpg", 32);
+var product33 = new Product(33, "Taiwan Wuyi Hongcha 2020", "Black Tea", "pictures/black-pics/taiwan-wuyi-hongcha-2020.jpg", 68);
+
+var product34 = new Product(34, "Golden Pu Erh", "Red Tea", "pictures/red-pics/golden-pu-erh.jpg", 43);
+var product35 = new Product(35, "Pu Erh Special", "Red Tea", "pictures/red-pics/pu-erh-special.jpg", 18);
+var product36 = new Product(36, "Pu Erh Organic", "Red Tea", "pictures/red-pics/pu-erh-organic.jpg", 16);
+var product37 = new Product(37, "pictures/red-pics/liuan-heicha-2009.jpg", "Red Tea", "pictures/red-pics/liuan-heicha-2009.jpg", 41);
+var product38 = new Product(38, "Dongguo Gushu Pu Erh 2020", "Red Tea", "pictures/red-pics/dongguo-gushu-pu-erh-2020.jpg", 115);
+var product39 = new Product(39, "Guangxi Liu Bao", "Red Tea", "pictures/red-pics/guangxi-liu-bao.jpg", 19);
+var product40 = new Product(40, "Sunyishun Liuan Heicha 1998", "Red Tea", "pictures/red-pics/sunyishun-liuan-heicha-1998.jpg", 165);
+var product41 = new Product(41, "Pu-erh Imperial Bio", "Red Tea", "pictures/red-pics/pu-erh-imperial-bio.jpg", 64);
+var product42 = new Product(42, "Four Gold Coins Liu Bao 2000", "Red Tea", "pictures/red-pics/four-gold-coins-liu-bao-2000.jpg", 64);
+
+let products = new Array(product1, product2, product3, product4, product5, product6, product7, product8, product9, product10,
+    product11, product12, product13, product14, product15, product16, product17, product18, product19, product20, product21, product22,
+    product23, product24, product25, product26, product27, product28, product29, product30, product31, product32, product33, product34,
+    product35, product36, product37, product38, product39, product40, product41, product42);
 
 for(let i = 0; i < 3; i++ ) {
     console.log(products[i]);
@@ -25,15 +70,17 @@ console.log(products);
 var template_item = document.getElementById('template_item');
 var grid_container = document.getElementById('grid_container');
 for(let i = 0; i < products.length; i++) {
+    
     var product = products[i];
     var clone = template_item.content.cloneNode(true);
-    clone.querySelector('.item_id').textContent=product.id;
-    clone.querySelector('p').textContent=product.name;
-    clone.querySelector('img').src = product.image;
-    clone.querySelector('span').textContent = product.price;
+    if(current_page == product.description ) {
+        clone.querySelector('.item_id').textContent=product.id;
+        clone.querySelector('p').textContent=product.name;
+        clone.querySelector('img').src = product.image;
+        clone.querySelector('span').textContent = product.price;
 
-
-    grid_container.appendChild(clone);
+        grid_container.appendChild(clone);
+    }
 }
 let cart = new Array;
 
